78887f41fa9acc4d4de92891ca7860af13b3c2b0
Furkan Gülşen <furkangulsenn19@gmail.com>
2026-03-06 03:05:17 +0300

    toplama örneği eklendi

---
+++ C-Temel-Ornekler/toplama.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int sayi1, sayi2, toplam;
+
+    printf("Iki sayi giriniz: ");
+    scanf("%d %d", &sayi1, &sayi2);
+
+    toplam = sayi1 + sayi2;
+
+    printf("Toplam = %d", toplam);
+
+    return 0;
+}
