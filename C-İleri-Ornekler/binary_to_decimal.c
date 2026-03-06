f4a2737fe76c7308191c1d4c464c0efbfd44db55
Furkan Gülşen <furkangulsenn19@gmail.com>
2026-03-06 04:05:05 +0300

    Add binary to decimal conversion program

---
+++ C-İleri-Ornekler/binary_to_decimal.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  
+    int i = 0, ondalik = 0, sayi;
+    int binary[32];
+  
+    printf("İkili sayı gir (en fazla 32 bit): ");
+  
+    while (scanf("%1d", &binary[i]) == 1) i++;
+
+    int basamak;
+  
+    for (basamak = 0; basamak < i; basamak++)
+        ondalik += binary[i-1-basamak] * pow(2, basamak);
+
+    printf("Ondalık karşılığı: %d\n", ondalik);
+
+  
+    return 0;
+}
